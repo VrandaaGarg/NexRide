d1e9af499257464d335e86621d56abcdf3385bb6
VrandaaGarg <vrandagarg2005@gmail.com>
2024-09-05 17:20:10 +0530

    data

--- client/src/Pages/RideGuide.jsx
+++ client/src/Pages/RideGuide.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import VehicleData from "../Data/VehicleData";
 
 function RideGuide() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <div className="">
+        <input type="text" />
+      </div>
+    </div>
+  );
 }
 
 export default RideGuide;
